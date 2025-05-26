43753568494f3cb2f7dc0436a0cd09fa281c5ec7
nbendasg <nick@bendas.info>
2025-05-26 12:56:36 -0400

    sloppy convert md readme to docs, fine tune needed

--- modules/proxmox-network/outputs.tf
+++ modules/proxmox-network/outputs.tf
@@ -2,7 +2,7 @@ output "bridges" {
   description = "Map of created Linux bridges"
   value       = proxmox_virtual_environment_network_linux_bridge.bridges
 }
-
+ 
 output "vlans" {
   description = "Map of created VLANs"
   value       = proxmox_virtual_environment_network_linux_vlan.vlans
--- modules/proxmox-storage/outputs.tf
+++ modules/proxmox-storage/outputs.tf
@@ -2,7 +2,7 @@ output "pools" {
   description = "Map of created storage pools"
   value       = proxmox_virtual_environment_storage.pools
 }
-
+ 
 output "volumes" {
   description = "Map of created storage volumes"
   value       = proxmox_virtual_environment_storage_volume.volumes
