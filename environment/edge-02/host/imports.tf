1c1d1caa29ca77331bc491e2a8b58db445589aa3
nbendasg <nick@bendas.info>
2025-06-08 22:06:55 -0400

    remove imports.tf from edge-02 as well

--- environment/edge-02/host/imports.tf
+++
@@ -1,9 +0,0 @@
-/**
- * Import the default vmbr0 bridge that the Proxmox installer
- * puts on every node. This will only take effect on the first run
- * when the bridge isn't in state yet.
- */
-import {
-  to = module.proxmox_network.proxmox_virtual_environment_network_linux_bridge.bridges["vmbr0"]
-  id = "pve-edge02:vmbr0"
-} 
