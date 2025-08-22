bc3cf2ba9fc0e2381bb0effecc1267c3d2d2a3d8
nbendasg <nick@bendas.info>
2025-08-22 12:32:31 -0400

    Add missing outputs for VM dependency

--- environment/edge-01/host/outputs.tf
+++ environment/edge-01/host/outputs.tf
@@ -1,4 +1,14 @@
 output "automation_token" {
   value     = module.proxmox_foundation.automation_user.token
   sensitive = true  # Mark as sensitive
+}
+
+output "api_url" {
+  description = "The Proxmox API endpoint URL"
+  value       = "https://${var.ip_address}:8006/api2/json"
+}
+
+output "hostname" {
+  description = "The hostname of the Proxmox node"
+  value       = var.hostname
 } 
