e0ec177f881ce39996a7c242261e2ea751f0ecf1
nbendasg <nick@bendas.info>
2025-08-22 12:33:07 -0400

    Fix duplicate output definition

--- environment/edge-01/host/outputs.tf
+++ environment/edge-01/host/outputs.tf
@@ -3,11 +3,6 @@ output "automation_token" {
   sensitive = true  # Mark as sensitive
 }
 
-output "api_url" {
-  description = "The Proxmox API endpoint URL"
-  value       = "https://${var.ip_address}:8006/api2/json"
-}
-
 output "hostname" {
   description = "The hostname of the Proxmox node"
   value       = var.hostname
